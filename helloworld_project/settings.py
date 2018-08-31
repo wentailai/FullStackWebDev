9a03c7b18c4750da627ff96ffb03dd808852b741
Tailai Wen <wentailai.jiuzhang@gmail.com>
2018-08-30 23:02:52 -0400

    Deploy to heroku

--- helloworld_project/settings.py
+++ helloworld_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8&#yav6_m*+=!8o)rd)w=y8o(*g)wlk=by)^gg$0sbs+_f-!d2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
